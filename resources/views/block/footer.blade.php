193e89c5f0e56b859456f2e9359de254c98f5f85
ngocha1301 <ngocha13012018@gmail.com>
2019-12-01 09:51:00 +0000

    them bachup

--- resources/views/block/footer.blade.php
+++ resources/views/block/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
     <hr>
-    <footer>
+    <footer class="container">
         <div class="row">
             <div class="col-md-12">
                 <p class="text-center">Copyright &copy; Your Website 2017</p>
